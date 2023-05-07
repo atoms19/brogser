962938afe9a247dd420e23e5158d6331e5ac3f36
atoms <74504022+atoms19@users.noreply.github.com>
2023-05-07 18:41:26 +0530

    Update main.js

--- home/main.js
+++ home/main.js
@@ -1,16 +1,22 @@
 weatherApiKey='4fbd7e056ad5fba4b7cdcd73ff8a1c12'
-lat=''
-lon=''
+
 
 
 
 function id(u){
   return document.getElementById(u)
 }
+
 homeSection=id('home')
+mainSection=document.querySelector('.main')
 popupSection=id('popup')
 shortcutNameIn=id('name-in')
 shortcutUrlIn=id('url-in')
+configMenuBtn=id('config-menu-btn')
+configMenuBtnClose=id('menu-close-btn')
+configMenu=id('config-menu')
+bgin=id('bg-input')
+weatherin=id('weather-input')
 
 id('shortcut-add-btn').addEventListener('click',()=>{
   homeSection.classList.add('hide')
@@ -38,18 +44,20 @@ window.onload=()=>{
     shortcutCreate(shortcut.label,shortcut.image)
   })
   
+  cityName=localStorage.getItem('city')||'mumbai'
+  weatherin.value=cityName
   
-  apiLink=`http://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=${weatherApiKey}`
+  updateWeather()
   
-   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-       lon = position.coords.longitude;
-      lat = position.coords.latitude;
-   apiLink=`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-`lon=${lon}&appid=${weatherApiKey}`
-    })
-  }
+  url=localStorage.getItem('bgimg')||`https://images.unsplash.com/photo-1681823853101-9a0d7e8be47b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80`
+  
+  mainSection.style.background=`url('${url}')`
+  bgalign()
+}
+shortcuts=[]
+
+function updateWeather(){
+  apiLink=`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherApiKey}`
   
   fetch(apiLink).then((response)=>{
    return response.json()
@@ -57,13 +65,15 @@ window.onload=()=>{
     console.log(data)
     id('temp').innerHTML=Math.floor(data.main.temp-273)+'⁰C'
     id('forcast').innerHTML=data.weather[0].description})
-  
-  
 }
-shortcuts=[]
 
 
 
+function bgalign(){
+  mainSection.style.backgroundSize='cover'
+  mainSection.style.backgroundRepeat='no-repeat'
+ 
+}
 
 
 
@@ -102,5 +112,37 @@ function search(s){
   s.submit()
 }
 
+configMenuBtn.addEventListener('click',()=>{
+ 
+    configMenu.classList.remove('hide')
+    homeSection.classList.add('hide')
+})
+configMenuBtnClose.addEventListener('click',()=>{
+ 
+    configMenu.classList.add('hide')
+    homeSection.classList.remove('hide')
+})
 
-``
+//background changer
+bgin.addEventListener('change',()=>{
+  filer=new FileReader()
+  url=filer.readAsDataURL(bgin.files[0])
+
+  
+  filer.onload=(e)=>{
+  document.querySelector('.main').style.background=`url('${e.target.result}')`
+  localStorage.setItem('bgimg',e.target.result)
+    bgalign()
+  }
+  
+  
+  console.log(url)
+})
+
+//weather changer
+weatherin.addEventListener('change',()=>{
+  cityName=weatherin.value
+  localStorage.setItem('city',cityName)
+updateWeather()
+  
+})
